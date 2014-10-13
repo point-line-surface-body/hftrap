2fa102027b6742169d8852eb628857a805660175
Ashwin Kumar <ashwin.kumar@tworoads.co.in>
2014-10-13 19:29:36 +0530

    Sim Strategy

--- InitCommon/strategy_desc.py
+++ InitCommon/strategy_desc.py
@@ -3,34 +3,32 @@ class StrategyLine():
         self.runtime_id_ = 0
         self.trading_start_ttime_t_ = 0
         self.trading_end_ttime_t_ = 0
-        self.trading_start_utc_mfm_=8*60*60*1000
-        self.trading_end_utc_mfm_=8*60*60*1000
-        self.p_dep_market_view_ = None
-        self.p_base_trader_ = None
-        self.exec_ = None
+        self.trading_start_utc_mfm_ = 8*60*60*1000
+        self.trading_end_utc_mfm_ = 8*60*60*1000
+        #self.p_dep_market_view_ = None
+        #self.p_base_trader_ = None
+        #self.exec_ = None
         self.strategy_full_line_ = ""
         self.dep_shortcode_ = "invalid"
-        self.strategy_name_= "invalid"
-        self.modelfilename_= "invalid"
-        self.paramfilename_= "invalid"
-        self.trading_manager_ = None
-        self.pair_exec_ = None
-
+        self.strategy_name_ = "invalid"
+        self.model_filename_ = "invalid"
+        self.param_filename_ = "invalid"
+        #self.trading_manager_ = None
+        #self.pair_exec_ = None
 
 class StrategyDesc():
     
     def __init__(self, _strategy_desc_filename_, tradingdate_):
         self.strategy_desc_filename_ = _strategy_desc_filename_
         self.strategy_vec_ = []
-        self.simconfig_filename_ = ""
+        #self.simconfig_filename_ = ""
         strategy_desc_file_ = open(self.strategy_desc_filename_)
-        for line in strategy_desc_file_ :
-            line - line.split("\n")
-            tokens_ = line.split()
-            if len(tokens_) >= 7 and tokens_[0] =="STRATEGYLINE" : # tokens[8] = traded zone, so skipping it
+        for line_ in strategy_desc_file_:
+            tokens_ = line_.split()
+            if len(tokens_) >= 7 and tokens_[0] == "STRATEGYLINE": # tokens[8] = traded zone, so skipping it
                 t_new_strategy_line_ = StrategyLine()
                 t_new_strategy_line_.dep_shortcode_ = tokens_[1]
-                t_new_strategy_line_.strategy_name_  =tokens_[2]
+                t_new_strategy_line_.strategy_name_ = tokens_[2]
                 t_new_strategy_line_.model_filename_ = tokens_[3]
                 t_new_strategy_line_.paramfilename_ = tokens_[4]
                 t_new_strategy_line_.trading_start_ttime_t_ = tokens_[5]
@@ -38,15 +36,14 @@ class StrategyDesc():
                 t_new_strategy_line_.trading_end_ttime_t_ = tokens_[6]
                 t_new_strategy_line_.trading_end_utc_mfm_= 0 #GetMsecsFromMidnightFromHHMMSS(tokens_[6])
                 t_new_strategy_line_.runtime_id_ = (int)(tokens_[7])
-                if len(tokens_):
-                    self.simconfig_filename_ = tokens_[7]
-                t_new_strategy_line_.strategy_full_line_ = line
+                #if len(tokens_):
+                #    self.simconfig_filename_ = tokens_[7]
+                t_new_strategy_line_.strategy_full_line_ = line_
                 for i in range(len(self.strategy_vec_)):
                     if self.trategy_vec_[i].runtime_id_ == t_new_strategy_line_.runtime_id_:
                         exit()
-                self.strategy_vec_.append( t_new_strategy_line_ ) ;
+                self.strategy_vec_.append(t_new_strategy_line_)
         
-    
     def GetMinStartTime(self):
         retval_ = self.strategy_vec_[0].trading_start_ttime_t_
         i = 1
@@ -77,8 +74,6 @@ class StrategyDesc():
         return True
     
     #Dont need this function ??
-    @staticmethod
-    def GetRollParam(_paramfile_,_tradingdate_ ):
-        return _paramfile_
-
-
+    #@staticmethod
+    #def GetRollParam(_paramfile_,_tradingdate_ ):
+    #    return _paramfile_
--- InitLogic/sim_strategy.py
+++ InitLogic/sim_strategy.py
@@ -6,61 +6,58 @@ from InitCommon.strategy_desc import *
 from CommonTradeUtils.watch import Watch
 from ModelMath.model_creator import ModelCreator
 from ExecLogic.base_trading import BaseTrading
+from InitCommon import strategy_desc
+from ExternalData.historical_dispatcher import HistoricalDispatcher
+from ExternalData import historical_dispatcher
+import OrderManager
 
 SECONDS_TO_PREP = 1800
 MIN_YYYYMMDD = 20090920
 MAX_YYYYMMDD = 20141225
 
-def termination_handler(signum):
-    exit()
-
-def ParseCommandLineParams(*argv):
-    return
-
 def __main__():
-    signal.signal(signal.SIGINT, termination_handler)
-    signal.signal(signal.SIGSEGV, termination_handler)
-    signal.signal(signal.SIGILL, termination_handler)
-    signal.signal(signal.SIGFPE, termination_handler)
-    livetrading_ = False
     tradingdate_ = 0
     progid_ = 0
     strategy_desc_filename_ = ""
-    tradesfilename_ = ""
-    home = expanduser("~")
-    network_account_info_filename_ = home +  BASESYSINFODIR + "TradingInfo/NetworkInfo/network_account_info_filename.txt"
-    market_model_index_ = 0
-    secs_to_prep_ = SECONDS_TO_PREP
-    strat_start_time_ =0
-    #HFSAT::SecurityNameIndexer & sec_name_indexer_ = HFSAT::SecurityNameIndexer::GetUniqueInstance ( ) ;
-    source_shortcode_vec_ = []
-    ors_needed_by_indicators_vec_ = []
-    dependant_shortcode_vec_ = []
-    sid_to_marketdata_needed_map_ = []
-    #ParseCommandLineParams ( livetrading_, tradingdate_, strategy_desc_filename_, progid_,network_account_info_filename_, market_model_index_, secs_to_prep_, strat_start_time_ )
-    strategy_desc_ = StrategyDesc(strategy_desc_filename_,tradingdate_)
+
+    source_shortcode_vec_ = [] # vector of all sources which we need data for or are trading
+    dependant_shortcode_ = None # separate vector for the sources which we have to made simtraders etc of
+    strategy_desc_ = StrategyDesc(strategy_desc_filename_, tradingdate_)
     watch_ = Watch(tradingdate_)
-    modelfilename_source_shortcode_vec_map_ = [] #map from modelfilename_ to source_shortcode_vec_ to pass to setup ModelMath as listener to market_data events
-    modelfilename_ors_needed_by_indicators_vec_map_ = [] #map from modelfilename_ to source_shortcode_vec_ to pass to setup ModelMath as listener to OrderRouting message events
-    for i in range(len(strategy_desc_.strategy_vec_)) :
-        if strategy_desc_.strategy_vec_[i].modelfilename_ not in modelfilename_source_shortcode_vec_map_:
-            if strategy_desc_.strategy_vec_[i].dep_shortcode_ not in dependant_shortcode_vec_:
-                dependant_shortcode_vec_.append(strategy_desc_.strategy_vec_[i].dep_shortcode_)
-            if strategy_desc_.strategy_vec_[i].dep_shortcode_ not in source_shortcode_vec_:
-                source_shortcode_vec_.append(strategy_desc_.strategy_vec_[i].dep_shortcode_)
-            ModelCreator.CollectShortCodes(watch_,strategy_desc_.strategy_vec_[i].modelfilename_,modelfilename_source_shortcode_vec_map_ [ strategy_desc_.strategy_vec_[i].modelfilename_ ],modelfilename_ors_needed_by_indicators_vec_map_ [ strategy_desc_.strategy_vec_[i].modelfilename_ ],False)
-            if modelfilename_source_shortcode_vec_map_ [ strategy_desc_.strategy_vec_[i].modelfilename_ ] not in source_shortcode_vec_:
-                source_shortcode_vec_.append(modelfilename_source_shortcode_vec_map_ [ strategy_desc_.strategy_vec_[i].modelfilename_ ])
-            if modelfilename_ors_needed_by_indicators_vec_map_ [ strategy_desc_.strategy_vec_[i].modelfilename_ ] not in ors_needed_by_indicators_vec_:
-                ors_needed_by_indicators_vec_.append(modelfilename_ors_needed_by_indicators_vec_map_ [ strategy_desc_.strategy_vec_[i].modelfilename_ ])
-            BaseTrading.CollectORSShortCodes(strategy_desc_.strategy_vec_[i].strategy_name_,strategy_desc_.strategy_vec_[i].dep_shortcode_, source_shortcode_vec_, ors_needed_by_indicators_vec_)
-            if strategy_desc_.strategy_vec_[i].dep_shortcode_ not in ors_needed_by_indicators_vec_ :
-                ors_needed_by_indicators_vec_.append(strategy_desc_.strategy_vec_[i].dep_shortcode_)
+    dependant_shortcode_ = strategy_desc_.strategy_desc[0].dep_shortcode_
+    shortcode_to_sid_map_ = {}
+    source_shortcode_vec_.appendUnique(strategy_desc_.strategy_vec_[0].dep_shortcode_)
+    ModelCreator.CollectShortCodes(strategy_vec_[0].model_filename_, source_shortcode_vec_)
+    for i in range(0, len(source_shortcode_)):
+        shortcode_to_sid_map_[source_shortcode_[i]] = i
+    sid_to_smv_ptr_map_ = []
+    
+    for i in range(0, len(source_shortcode)):
+        t_smv_ = SecurityMarketView(watch_, source_shortcode_[i], i)
+        sid_to_smv_ptr_map_.append(t_smv_)
+
+    historical_dispatcher_ = HistoricalDispatcher()
+
+    for i in range(0, len(source_shortcode_vec_)):
+        t_file_source = FileSource(source_shortcode_vec_[i], tradingdate_, i)
+        historical_dispatcher_.AddExternalDataListener(t_file_source)
+
+    sim_market_maker_ = PriceLevelSimMarketMaker(watch_, sid_to_smv_ptr_map_[0])
+    strategy_desc_.strategy_vec_[0].dep_market_view_ = sid_to_smv_ptr_map_[0]
+    strategy_desc_.strategy_vec_[0].base_trader_ = sim_market_maker
+
+    order_manager_ = OrderManager(watch_, sid_to_shortcode_ptr_map_[0], base_trader)
+    base_pnl = BasePnl(watch_, order_manager_, sid_to_shortcode_ptr_map_[0])
 
 
+    base_model_math_ = ModelCreator.CreateModelMathComponent(watch_, model_filename_)
 
+    base_model_math_.AddListener(strategy_desc_.strategy_vec_[0].exec_)
+    strategy_desc_.strategy_vec_[0].exec_.SetModelMathComponent(base_model_math_)
 
-    return
+    market_update_manager_.start();
 
-if __name__ == "__main__":
-    __main__()
+    data_seek_time_ = strategy_desc_.GetMinStartTime() # subtract some preparation time
+    historical_dispatcher_.SeekHistFileSourcesTo(data_seek_time)
+    hist_disp_end_time = strategy_desc_.GetMaxEndTime() # add 1 hour
+    historical_dispatcher_.RunHist(hist_disp_end_time)
