485d426c6453c5dbd2dadd15dc865e845dba5a29
harlalka <aashay.harlalka@gmail.com>
2014-10-19 11:50:57 +0530

    _

--- CDef/security_definitions.py
+++ CDef/security_definitions.py
@@ -4,13 +4,18 @@ class ContractSpecification(object):
         self.numbers_to_dollars_ = 1.0
         self.exch_source_ = "INVALID"
         self.min_order_size_ = 1
-        if len(argv) == 4 :
+        if len(argv) == 3 :
             self.min_price_increment_ = (float)(argv[0])
             self.numbers_to_dollars_ = (float)(argv[1])
-            self.exch_source_ = argv[2]
-            self.min_order_size_ = (int)(argv[3])
+            self.min_order_size_ = (int)(argv[2])
             
 class SecurityDefinitions(object):
+    contract_specification_map_ = {}
+    
+    def __init__ (self, trading_date_):
+        self.trading_date_ = trading_date_
+        if "ZN_0" not in SecurityDefinitions.contract_specification_map_.keys() :
+            SecurityDefinitions.contract_specification_map_["ZN_0"] = ContractSpecification(0.015625, 1000, 1)
     
     @staticmethod
     def GetContractMinPriceIncrement(_shortcode_, _date_):
